0beb8982a7742f539a77325240a4a66e6a3c6ff1
Patrick <pmoore4181@gmail.com>
2017-06-18 12:21:13 -0500

    added js file. search through bars and put in html. still need to do yelp api stuff

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,105 +1,80 @@
 $(document).ready(function(){
 
-function bar(barName, location, startTime, endTime, priceRange, daysWithSpecials, genre, drinkType, picture) {
-	this.barName = barName;
-	this.location = location;
-	this.startTime = startTime;
-	this.endTime = endTime;
-	this.priceRange = priceRange;
-	this.daysWithSpecials = daysWithSpecials;
-	this.genre = genre;
-	this.drinkType= drinkType;
-	this.picture = picture;
+function bar(barName, location, startTime, endTime, priceRange, daysWithSpecials, genre, drinkType, picture, yelpReview) {
+	this.barName = barName,
+	this.location = location,
+	this.startTime = startTime,
+	this.endTime = endTime,
+	this.priceRange = priceRange,
+	this.daysWithSpecials = daysWithSpecials,
+	this.genre = genre,
+	this.drinkType= drinkType,
+	this.picture = picture,
+	this.yelpReview = yelpReview
 }
 
-var libertineBar = new bar ('Libertine', '2101 Greenville Ave', '5', '7', '2', ['mon', 'tue', 'wed', 'thu', 'fri'],  'full bar', '<img class="barPicture" src="Libertine.jpeg"/>');
+var libertineBar = new bar ('Libertine', '2101 Greenville Ave', '5', '7', '2', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], 'Restaurant', 'Full Bar', '<img class="barPicture" src="photos/libertineD.jpg"/>', );
 
-var truckYardBar = new bar ('Truck Yard', '5624 Sears St', '5', '7', '2', ['mon', 'wed', 'fri'], 'outdoor', 'full bar', '<img src="truckyard.jpg" />');
+var truckYardBar = new bar ('Truck Yard', '5624 Sears St', '5', '7', '2', ['Monday', 'Wednesday', 'Friday'], 'outdoor', 'Full Bar', '<img class="barPicture" src="photos/Truck-Yard-in-Dallas_201240.jpg"/>');
 
-var dallasBeerKitchenBar = new bar ('Dallas Beer Kitchen', '1802 Greenville Ave Ste 110', '5', '7', '1', ['fri', 'sat'], 'dive', 'beer');
+var dallasBeerKitchenBar = new bar ('Dallas Beer Kitchen', '1802 Greenville Ave Ste 110', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="photos/dallasBearKitchen.jpg"/>');
 
-var shipsLoungeBar = new bar ('Ships Lounge', '1613 Greenville Ave', '5', '7', '1', ['fri', 'sat'], 'pool hall', 'beer');
+var shipsLoungeBar = new bar ('Ships Lounge', '1613 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Pool Hall', 'Beer', '<img class="barPicture" src="photos/ships3.png"/>');
 
-var theOldMonkBar = new bar ('The Old Monk', '2847 N Henderson Ave', '5', '7', '2', ['fri', 'sat'], 'pub', 'full bar');
+var theOldMonkBar = new bar ('The Old Monk', '2847 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Pub', 'Full Bar', '<img class="barPicture" src="photos/theoldMonk.jpg"/>');
 
-var veritasWineRoomBar = new bar ('Veritas Wine Room', '2323 N Henderson Ave', '5', '7', '2', ['fri', 'sat'], 'wine bar', 'wine');
+var veritasWineRoomBar = new bar ('Veritas Wine Room', '2323 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Wine Bar', 'Wine', '<img class="barPicture" src="photos/veritasWine.jpg"/>');
 
-var singleWideBar = new bar ('Single Wide', '2110 Greenville Ave', '5', '7', '1', ['fri', 'sat'], 'dive', 'full bar');
+var singleWideBar = new bar ('Single Wide', '2110 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Full Bar', '<img class="barPicture" src="photos/singlewide.jpg"/>');
 
-var lgTapsBar = new bar ('LG Taps', '3619 Greenville Ave', '5', '7', '1', ['fri', 'sat'], 'restaurant', 'craft beer');
+var lgTapsBar = new bar ('LG Taps', '3619 Greenville Ave', '5', '7', '1', ['Monday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="photos/lgtaps"/>');
 
-var thePorchBar = new bar ('The Porch', '2912 N Henderson Ave', '5', '7', '2', ['fri', 'sat'], 'restaurant', 'craft beer');
+var thePorchBar = new bar ('The Porch', '2912 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="photos/porch.jpg"/>');
 
-var victorTangosBar = new bar ('Victor Tangos', '3001 N Henderson Ave', '5', '7', '2', ['fri', 'sat'], 'cocktail bar', 'cocktails');
+var victorTangosBar = new bar ('Victor Tangos', '3001 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Cocktail Bar', 'Cocktails', '<img class="barPicture" src="photos/victortangos.jpg"/>');
 
-var theOldCrowBar = new bar ('The Old Crow', '1911 Greenville Ave', '5', '7', '1', ['fri', 'sat'], 'dive', 'beer');
+var theOldCrowBar = new bar ('The Old Crow', '1911 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="photos/oldcrow.jpg"/>');
 
-var rapscallionBar = new bar ('Rapscallion', '2023 Greenville Ave Ste 110', '5', '7', '2', ['fri', 'sat'], 'restaurant', 'beer');
+var rapscallionBar = new bar ('Rapscallion', '2023 Greenville Ave Ste 110', '5', '7', '2', ['Friday', 'Saturday'], 'Restaurant', 'Beer', '<img class="barPicture" src="photos/rapscallion.JPG"/>');
 
 var barArray = [];
 barArray.push(libertineBar, truckYardBar, dallasBeerKitchenBar, shipsLoungeBar, theOldMonkBar, veritasWineRoomBar, singleWideBar, 
 	lgTapsBar, thePorchBar, victorTangosBar, theOldCrowBar, rapscallionBar);
 
-console.log(barArray[0].picture);
 
 
-// search by day of the week
-function checkDaysWithSpecials(searchDate){
-	for(i = 0; i < 5; i++) {
-		for(j=0; j < barArray.length; j++) {
-			if(barArray[j].daysWithSpecials[i] === searchDate) {
+$(".day").on("click", function(){
+	var searchDate = $(this).attr("value");
 
-				var newDiv = $("<div>");
-				$(newDiv).append(barArray[j].barName + "<br>");
-				$(newDiv).append(barArray[j].location);
-				$(newDiv).append("Hours: " + barArray[j].startTime + " to " + barArray[j].endTime + "<br>");
-				$(newDiv).append("Price: " + barArray[j].priceRange + "<br>");
-				$(newDiv).append(barArray[j].daysWithSpecials.join(", ").toString() + "<br>");
-				$(newDiv).append(barArray[j].genre + "<br>");
-				$(newDiv).append(barArray[j].drinkType + "<br>");
-				$(newDiv).append(barArray[j].picture);
-				$(newDiv).append("<br><br>");
-				$(".barSection").append($(newDiv));
+	// replace old search and add day to top of section
+	$(".searchResults").html("<h1>" + searchDate + " Happy Hours</h1>");
 
-			}
-		} 
+	// search by day of the week
+	function checkDaysWithSpecials(){
+		for(i = 0; i < barArray.length; i++) {
+			for(j=0; j < 6; j++) {
+				if(barArray[i].daysWithSpecials[j] === searchDate) {
+
+					// create new div and apply to #address section
+					var newDiv = $("<div class='result'>");
+					$(newDiv).append("<div class='barName'>" + barArray[i].barName + "</div>");
+					$(newDiv).append("<div class='barLocation'>" + barArray[i].location + "</div>");
+					$(newDiv).append("<div class='barTime'>Hours: " + barArray[i].startTime + " to " + barArray[i].endTime + "</div>");
+					$(newDiv).append("<div class='barPrice'>Price: " + barArray[i].priceRange + "</div>");
+					$(newDiv).append("<div class='barSpecials'>" + barArray[i].daysWithSpecials.join(", ").toString() + "</div>");
+					$(newDiv).append("<div class='barGenre'>" + barArray[i].genre + "</div>");
+					$(newDiv).append("<div class='barDrinks'>" + barArray[i].drinkType + "</div>");
+					$(newDiv).append(barArray[i].picture);
+					$(newDiv).append("<br><br>");
+					$(".searchResults").append($(newDiv));
+
+
+				}
+			} 
+		}
 	}
-}
-checkDaysWithSpecials("mon");
-
-
-// ========================================================================================
-// // search by price
-// function checkPrice(searchPrice){
-// 	for(i = 0; i < barArray.length; i++) {
-// 		if(searchPrice === barArray[i].priceRange) {
-
-
-// 			var newDiv = $("<div>");
-// 			$(newDiv).append(barArray[i].barName + "<br>");
-// 			$(newDiv).append(barArray[i].location);
-// 			$(newDiv).append("Hours: " + barArray[i].startTime + " to " + barArray[i].endTime + "<br>");
-// 			$(newDiv).append("Price: " + barArray[i].priceRange + "<br>");
-// 			$(newDiv).append(barArray[i].daysWithSpecials.toString() + "<br>");
-// 			$(newDiv).append(barArray[i].genre + "<br>");
-// 			$(newDiv).append(barArray[i].drinkType);
-// 			$(newDiv).append("<br><br>");
-// 			$(".barSection").append($(newDiv));
-
-
-
-// 			console.log(barArray[i].barName);
-// 			console.log(barArray[i].location);
-// 			console.log("Hours: " + barArray[i].startTime + " to " + barArray[i].endTime);
-// 			console.log(barArray[i].priceRange);
-// 			console.log(barArray[i].daysWithSpecials.toString());
-// 			console.log(barArray[i].genre);
-// 			console.log(barArray[i].drinkType);
-// 			console.log("===================================");
-// 		}
-// 	}
-// }
-// checkPrice("2");
+	checkDaysWithSpecials();
+})
 
 
 
