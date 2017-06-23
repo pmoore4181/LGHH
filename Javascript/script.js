199a7667be083939effb624ac82c8ea9f0cd8fc3
Patrick <pmoore4181@gmail.com>
2017-06-23 12:27:18 -0500

    added link to logo, divider line between bar lists, changed price, added footer

--- Javascript/script.js
+++ Javascript/script.js
@@ -13,29 +13,29 @@ function bar(barName, location, startTime, endTime, priceRange, daysWithSpecials
 	this.twitter = twitter
 }
 
-var libertineBar = new bar ('Libertine', '2101 Greenville Ave', '5', '7', '2', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], 'Restaurant', 'Full Bar', '<img class="barPicture" src="../corrected/libertineD.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/libertinebar" data-tweet-limit="1"> Tweets by @libertinebar </a>');
+var libertineBar = new bar ('Libertine', '2101 Greenville Ave', '5', '7', 'Moderate', ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], 'Restaurant', 'Full Bar', '<img class="barPicture" src="../corrected/libertineD.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/libertinebar" data-tweet-limit="1"> Tweets by @libertinebar </a>');
 
-var truckYardBar = new bar ('Truck Yard', '5624 Sears St', '5', '7', '2', ['Monday', 'Wednesday', 'Friday'], 'outdoor', 'Full Bar', '<img class="barPicture" src="../corrected/Truck-Yard-in-Dallas_201240.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/texastruckyard" data-tweet-limit="1"> Tweets by @texastruckyard </a>');
+var truckYardBar = new bar ('Truck Yard', '5624 Sears St', '5', '7', 'Moderate', ['Monday', 'Wednesday', 'Friday'], 'outdoor', 'Full Bar', '<img class="barPicture" src="../corrected/Truck-Yard-in-Dallas_201240.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/texastruckyard" data-tweet-limit="1"> Tweets by @texastruckyard </a>');
 
-var dallasBeerKitchenBar = new bar ('Dallas Beer Kitchen', '1802 Greenville Ave Ste 110', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="../corrected/dallasBearKitchen.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/FollowDBK" data-tweet-limit="1"> Tweets by @FollowDBK </a>');
+var dallasBeerKitchenBar = new bar ('Dallas Beer Kitchen', '1802 Greenville Ave Ste 110', '5', '7', 'Cheap', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="../corrected/dallasBearKitchen.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/FollowDBK" data-tweet-limit="1"> Tweets by @FollowDBK </a>');
 
-var shipsLoungeBar = new bar ('Ships Lounge', '1613 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Pool Hall', 'Beer', '<img class="barPicture" src="../corrected/ships.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/theship171" data-tweet-limit="1"> Tweets by @theship171 </a>');
+var shipsLoungeBar = new bar ('Ships Lounge', '1613 Greenville Ave', '5', '7', 'Cheap', ['Friday', 'Saturday'], 'Pool Hall', 'Beer', '<img class="barPicture" src="../corrected/ships.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/theship171" data-tweet-limit="1"> Tweets by @theship171 </a>');
 
-var theOldMonkBar = new bar ('The Old Monk', '2847 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Pub', 'Full Bar', '<img class="barPicture" src="../corrected/dallas-the-old-monk.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/OldMonkDallas" data-tweet-limit="1"> Tweets by @OldMonkDallas </a>');
+var theOldMonkBar = new bar ('The Old Monk', '2847 N Henderson Ave', '5', '7', 'Moderate', ['Friday', 'Saturday'], 'Pub', 'Full Bar', '<img class="barPicture" src="../corrected/dallas-the-old-monk.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/OldMonkDallas" data-tweet-limit="1"> Tweets by @OldMonkDallas </a>');
 
-var veritasWineRoomBar = new bar ('Veritas Wine Room', '2323 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Wine Bar', 'Wine', '<img class="barPicture" src="../corrected/veritasWine.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/veritasdallas" data-tweet-limit="1"> Tweets by @veritasdallas </a>');
+var veritasWineRoomBar = new bar ('Veritas Wine Room', '2323 N Henderson Ave', '5', '7', 'Moderate', ['Friday', 'Saturday'], 'Wine Bar', 'Wine', '<img class="barPicture" src="../corrected/veritasWine.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/veritasdallas" data-tweet-limit="1"> Tweets by @veritasdallas </a>');
 
-var singleWideBar = new bar ('Single Wide', '2110 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Full Bar', '<img class="barPicture" src="../corrected/singlewide.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/dmillennials" data-tweet-limit="1"> Tweets by @dmillennials </a>');
+var singleWideBar = new bar ('Single Wide', '2110 Greenville Ave', '5', '7', 'Cheap', ['Friday', 'Saturday'], 'Dive', 'Full Bar', '<img class="barPicture" src="../corrected/singlewide.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/dmillennials" data-tweet-limit="1"> Tweets by @dmillennials </a>');
 
-var lgTapsBar = new bar ('LG Taps', '3619 Greenville Ave', '5', '7', '1', ['Monday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="../corrected/lgtaps.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/LGTAPSTAVERN" data-tweet-limit="1"> Tweets by @LGTAPSTAVERN </a>');
+var lgTapsBar = new bar ('LG Taps', '3619 Greenville Ave', '5', '7', 'Cheap', ['Monday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="../corrected/lgtaps.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/LGTAPSTAVERN" data-tweet-limit="1"> Tweets by @LGTAPSTAVERN </a>');
 
-var thePorchBar = new bar ('The Porch', '2912 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="../corrected/porch.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/porchrestaurant" data-tweet-limit="1"> Tweets by @porchrestaurant </a>');
+var thePorchBar = new bar ('The Porch', '2912 N Henderson Ave', '5', '7', 'Moderate', ['Friday', 'Saturday'], 'Restaurant', 'Craft Beer', '<img class="barPicture" src="../corrected/porch.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/porchrestaurant" data-tweet-limit="1"> Tweets by @porchrestaurant </a>');
 
-var victorTangosBar = new bar ('Victor Tangos', '3001 N Henderson Ave', '5', '7', '2', ['Friday', 'Saturday'], 'Cocktail Bar', 'Cocktails', '<img class="barPicture" src="../corrected/victortangos.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/Victor_Tangos" data-tweet-limit="1"> Tweets by @Victor_Tangos </a>');
+var victorTangosBar = new bar ('Victor Tangos', '3001 N Henderson Ave', '5', '7', 'Moderate', ['Friday', 'Saturday'], 'Cocktail Bar', 'Cocktails', '<img class="barPicture" src="../corrected/victortangos.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/Victor_Tangos" data-tweet-limit="1"> Tweets by @Victor_Tangos </a>');
 
-var theOldCrowBar = new bar ('The Old Crow', '1911 Greenville Ave', '5', '7', '1', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="../corrected/oldcrow.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/oldcrowbarnf" data-tweet-limit="1"> Tweets by @oldcrowbarnf </a>');
+var theOldCrowBar = new bar ('The Old Crow', '1911 Greenville Ave', '5', '7', 'Cheap', ['Friday', 'Saturday'], 'Dive', 'Beer', '<img class="barPicture" src="../corrected/oldcrow.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/oldcrowbarnf" data-tweet-limit="1"> Tweets by @oldcrowbarnf </a>');
 
-var rapscallionBar = new bar ('Rapscallion', '2023 Greenville Ave Ste 110', '5', '7', '2', ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], 'Restaurant', 'Beer', '<img class="barPicture" src="../corrected/rapscallion.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/RpscllnDallas" data-tweet-limit="1"> Tweets by @RpscllnDallas </a>');
+var rapscallionBar = new bar ('Rapscallion', '2023 Greenville Ave Ste 110', '5', '7', 'Moderate', ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], 'Restaurant', 'Beer', '<img class="barPicture" src="../corrected/rapscallion.png" style="width:350px; height: 350px"/>', '<a class="twitter-timeline" href="https://twitter.com/RpscllnDallas" data-tweet-limit="1"> Tweets by @RpscllnDallas </a>');
 
 var barArray = [];
 barArray.push(libertineBar, truckYardBar, dallasBeerKitchenBar, shipsLoungeBar, theOldMonkBar, veritasWineRoomBar, singleWideBar, 
@@ -67,7 +67,7 @@ $(".day").on("click", function(){
 					$(newDiv).append("<div class='barGenre'>" + barArray[i].genre + "</div>");
 					$(newDiv).append("<div class='barDrinks'>" + barArray[i].drinkType + "</div><br>");
 					$(newDiv).append(barArray[i].picture + "<br>");
-					$(newDiv).append("<br><br>");
+					$(newDiv).append("<br><br><hr id='hr'>");
 					$('#resultRow').append(newDiv);
 
 					//Inserting Twitter API
@@ -89,7 +89,6 @@ $(".day").on("click", function(){
 					    }(document, "script", "twitter-wjs"));
 					$(rightSideDiv).append(barArray[i].twitter);
 					$('#resultRow').append($(rightSideDiv));
-					$('#resultRow').append("<hr>");
 				}
 			} 
 		}
