39b2c64a5e7c807009d65615ba8eeec8d5a4b2c4
Patrick <pmoore4181@gmail.com>
2017-07-26 00:21:50 -0500

    changed js route

--- sample.js
+++ sample.js
@@ -10,14 +10,15 @@ app.use(express.static('bower_components')); // contains bootstrap stuff
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+// app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
+app.set('js', __dirname + '/javascript');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, "/views/mainpage.html"));
+    response.sendFile(path.join(__dirname, "views/mainpage.html"));
 });
 
 app.listen(app.get('port'), function() {
